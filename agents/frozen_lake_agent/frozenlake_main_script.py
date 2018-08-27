5d026a8979c7d039a9ebbf76f5ba97ae0c0bac61
jbarsce <jbarsce@gmail.com>
2018-08-27 14:13:22 -0300

    Agregada curva de pasos por episodio

--- agents/frozen_lake_agent/frozenlake_main_script.py
+++ agents/frozen_lake_agent/frozenlake_main_script.py
@@ -33,23 +33,41 @@ agent.init_agent()
 # reinicializa el conocimiento del agente
 agent.restart_agent_learning()
 
-# run corre el agente devuelve la cantidad promedio de pasos por episodio
+# se realiza la ejecución del agente
 avg_steps_per_episode = agent.run()
 
-# se muestra la curva de convergencia
+# se muestra la curva de convergencia de las recompensas
 episode_rewards = np.array(agent.reward_of_episode)
-plt.scatter(np.array(range(0, len(episode_rewards))), episode_rewards)
-plt.title('Recompensa')
+plt.scatter(np.array(range(0, len(episode_rewards))), episode_rewards, s=0.7)
+plt.title('Recompensa por episodio')
 plt.show()
 
-# se suaviza la curva de aprendizaje
+# se suaviza la curva de convergencia
 episode_number = np.linspace(1, len(episode_rewards) + 1, len(episode_rewards) + 1)
 acumulated_rewards = np.cumsum(episode_rewards)
 
 reward_per_episode = [acumulated_rewards[i] / episode_number[i] for i in range(len(acumulated_rewards))]
 
 plt.plot(reward_per_episode)
-plt.title('Recompensa por episodio')
+plt.title('Recompensa acumulada por episodio')
+plt.show()
+
+# ---
+
+# se muestra la curva de aprendizaje de los pasos por episodio
+episode_steps = np.array(agent.timesteps_of_episode)
+plt.plot(np.array(range(0, len(episode_steps))), episode_steps)
+plt.title('Pasos (timesteps) por episodio')
+plt.show()
+
+# se suaviza la curva de aprendizaje
+episode_number = np.linspace(1, len(episode_steps) + 1, len(episode_steps) + 1)
+acumulated_steps = np.cumsum(episode_steps)
+
+steps_per_episode = [acumulated_steps[i] / episode_number[i] for i in range(len(acumulated_steps))]
+
+plt.plot(steps_per_episode)
+plt.title('Pasos (timesteps) acumulados por episodio')
 plt.show()
 
 # Se muestra el agente con su política óptima
