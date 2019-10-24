43a03e811d11cfeef9f3ba404b715078a35551ad
Juan Cruz Barsce <jbarsce@gmail.com>
2019-10-24 16:01:35 -0300

    Third/Fourth lectures ready

--- agents/cart_pole_approx_vf/CartPole_SGD_Poly_Dual.py
+++ agents/cart_pole_approx_vf/CartPole_SGD_Poly_Dual.py
@@ -1,140 +1,217 @@
 # Inspired by https://keon.io/deep-q-learning/
 
-import random
 import gym
 import math
 import numpy as np
-from collections import deque
-from sklearn.linear_model import SGDRegressor
+import torch
+import torch.nn as nn
+
+from agents.utils.memory.ReplayMemory import ReplayMemory
+from agents.utils.memory.Transition import Transition
 from sklearn.preprocessing import PolynomialFeatures
 
 
-class SGDPolyDualCartPoleSolver:
-    def __init__(self, env, n_episodes=1000, max_env_steps=None, gamma=0.5, epsilon=1.0, epsilon_min=0.01,
-                 epsilon_decay=0.005, alpha=0.001, batch_size=32, c=10, debug=False):
-        self.debug = debug
-        self.memory = deque(maxlen=100000)
+class LinearModel(nn.Module):
+
+    def __init__(self, _input_size: int, _output_size: int):
+        super(LinearModel, self).__init__()
+        self.output = nn.Linear(_input_size, _output_size)
+
+        # init weights
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                m.bias.data.fill_(0.01)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = self.output(x)
+        return x
+
+
+class PolyDualCartPoleSolver:
+    def __init__(self, env, n_episodes=1000, max_env_steps=None, gamma=0.5,
+                 epsilon=1.0, epsilon_min=0.01, epsilon_log_decay=0.005, alpha=1e-3,
+                 memory_size=10000, batch_size=256, c=10, render=False, debug=False):
+        self.memory = ReplayMemory(capacity=memory_size)
         self.env = env
 
         # hyper-parameter setting
         self.gamma = gamma
         self.epsilon = epsilon
         self.epsilon_min = epsilon_min
-        self.epsilon_decay = epsilon_decay
+        self.epsilon_decay = epsilon_log_decay
         self.alpha = alpha
         self.n_episodes = n_episodes
         self.batch_size = batch_size
         self.c = c
-
-        self.feature_tuning = PolynomialFeatures(interaction_only=True)
-
         if max_env_steps is not None:
             self.env._max_episode_steps = max_env_steps
+        self.observation_space_size = env.observation_space.shape[0]
+        self.action_space_size = env.action_space.n
 
-        # Init model
-        self.model = SGDRegressor(
-                alpha=self.alpha,
-                learning_rate='optimal',
-                shuffle=False,
-                warm_start=True)
-
-        # Init dual model
-        self.model2 = SGDRegressor(
-            alpha=self.alpha,
-            learning_rate='optimal',
-            shuffle=False,
-            warm_start=True)
-
+        self.feature_tuning = PolynomialFeatures(interaction_only=True)
         # Initialize feature tunning
-        self.feature_tuning.fit(np.reshape(np.hstack((self.env.reset(), 0)), [1, 5]))
-        # Initialize model
-        self.model.partial_fit(self.preprocess_state(self.env.reset(), 0), [0])
-        # Initialize dual model
-        self.model2.partial_fit(self.preprocess_state(self.env.reset(), 0), [0])
+        self.feature_tuning.fit(self.env.reset().reshape((1, 4)))
 
-    def remember(self, state, action, reward, next_state, done):
-        """In this method, the (s, a, r, s') tuple is stored in the memory"""
-        self.memory.append((state, action, reward, next_state, done))
+        self.render = render
+        self.debug = debug
+        if debug:
+            self.loss_list = []
+        # if gpu is to be used
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Init model 1
+        # Use the nn package to define our model as a sequence of layers. nn.Sequential
+        # is a Module which contains other Modules, and applies them in sequence to
+        # produce its output. Each Linear Module computes output from input using a
+        # linear function, and holds internal Tensors for its weight and bias.
+        # After constructing the model we use the .to() method to move it to the
+        # desired device.
+        self.model = LinearModel(self.feature_tuning.n_output_features_, self.action_space_size).to(self.device)
+        self.model.train()
+        self.target = LinearModel(self.feature_tuning.n_output_features_, self.action_space_size).to(self.device)
+        self.target.load_state_dict(self.model.state_dict())
+        self.target.eval()
+
+        # The nn package also contains definitions of popular loss functions; in this
+        # case we will use Mean Squared Error (MSE) as our loss function. Setting
+        # reduction='sum' means that we are computing the *sum* of squared errors rather
+        # than the mean; this is for consistency with the examples above where we
+        # manually compute the loss, but in practice it is more common to use mean
+        # squared error as a loss by setting reduction='elementwise_mean'.
+        self.loss_fn = torch.nn.MSELoss()
+
+        # Use the optim package to define an Optimizer that will update the weights of
+        # the model for us. Here we will use Adam; the optim package contains many other
+        # optimization algoriths. The first argument to the Adam constructor tells the
+        # optimizer which Tensors it should update.
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=alpha)
+
+    def preprocess_state(self, state):
+        """State and action are stacked horizontally and its features are combined as a polynomial to be passed as an
+        input of the approximator"""
+
+        # poly_state converts the horizontal stack into a combination of its parameters i.e.
+        # [1, s_1, s_2, s_3, s_4, a_1, s_1 s_2, s_1 s_3, ...]
+        poly_state = self.feature_tuning.transform(state.reshape((1, 4)))
+        return poly_state[0]
 
     def choose_action(self, state, epsilon):
         """Chooses the next action according to the model trained and the policy"""
 
-        qsa = np.asarray([self.model.predict(self.preprocess_state(state, a)) for a in range(self.env.action_space.n)]).flatten()
-        return self.env.action_space.sample() if (np.random.random() <= epsilon) else \
-            np.argmax(qsa)  # exploits the current knowledge if the random number > epsilon, otherwise explores
+        # exploits the current knowledge if the random number > epsilon, otherwise explores
+        if np.random.random() <= epsilon:
+            return self.env.action_space.sample()
+        else:
+            with torch.no_grad():
+                q = self.model(state)
+                argmax = torch.argmax(q)
+                return argmax.item()
 
     def get_epsilon(self, episode):
         """Returns an epsilon that decays over time until a minimum epsilon value is reached; in this case the minimum
         value is returned"""
         return max(self.epsilon_min, self.epsilon * math.exp(-self.epsilon_decay * episode))
 
-    def preprocess_state(self, state, action):
-        """State and action are stacked horizontally and its features are combined as a polynomial to be passed as an
-        input of the approximator"""
-
-        # poly_state converts the horizontal stack into a combination of its parameters i.e.
-        # [1, s_1, s_2, s_3, s_4, a_1, s_1 s_2, s_1 s_3, ...]
-        poly_state = self.feature_tuning.transform(np.reshape(np.hstack((state, action)), [1, 5]))
-        return poly_state
-
-    def replay(self, batch_size):
+    def replay(self):
         """Previously stored (s, a, r, s') tuples are replayed (that is, are added into the model). The size of the
         tuples added is determined by the batch_size parameter"""
 
-        x_batch, y_batch = [], []
-        minibatch = random.sample(self.memory, min(len(self.memory), batch_size))
+        transitions, _ = self.memory.sample(self.batch_size)
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_next_states = torch.stack([s for s in batch.next_state if s is not None])
 
-        for state, action, reward, next_state, done in minibatch:
-            # q(s', a) is predicted by model 2
-            qsa_s_prime = np.asarray([self.model2.predict(self.preprocess_state(next_state, a))
-                                      for a in range(self.env.action_space.n)])
+        non_final_mask = torch.stack(batch.done)
+        state_batch = torch.stack(batch.state)
+        action_batch = torch.stack(batch.action)
+        reward_batch = torch.stack(batch.reward)
 
-            qsa_s = reward if done \
-                else reward + self.gamma * np.max(qsa_s_prime)
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.model(state_batch).gather(1, action_batch)
 
-            x_batch.append(self.preprocess_state(state, action)[0])
-            y_batch.append(qsa_s)
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        with torch.no_grad():
+            next_state_values = torch.zeros(self.batch_size, device=self.device)
+            next_state_values[non_final_mask] = self.target(non_final_next_states).max(1)[0].detach()
+            # Compute the expected Q values
+            expected_state_action_values = reward_batch + self.gamma * next_state_values
 
-        # the replayed experience is fit into model 1
-        self.model.partial_fit(np.array(x_batch), np.array(y_batch))
+        # Compute loss
+        loss = self.loss_fn(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        if self.debug:
+            self.loss_list.append(loss)
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.model.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
 
     def run(self):
         """Main loop that controls the execution of the agent"""
 
-        scores100 = deque(maxlen=100)
         scores = []
+        mean_scores = []
         j = 0  # used for model2 update every c steps
         for e in range(self.n_episodes):
             state = self.env.reset()
+            state = torch.tensor(self.preprocess_state(state), device=self.device, dtype=torch.float)
             done = False
-            cum_reward = 0  # Maintains the cumulative reward during de episode
-
+            cum_reward = 0
             while not done:
-                action = self.choose_action(state, self.get_epsilon(e))
+                action = self.choose_action(
+                    state,
+                    self.get_epsilon(e))
                 next_state, reward, done, _ = self.env.step(action)
-                self.remember(state, action, reward, next_state, done)
+                next_state = torch.tensor(self.preprocess_state(next_state), device=self.device, dtype=torch.float)
 
-                self.replay(self.batch_size)
-
-                state = next_state
                 cum_reward += reward
-                j += 1
+                self.memory.push(
+                    state,  # Converted to tensor before
+                    torch.tensor([action], device=self.device),
+                    None if done else next_state,
+                    torch.tensor(reward, device=self.device).clamp_(-1, 1),
+                    torch.tensor(not done, device=self.device, dtype=torch.bool))
 
+                if self.memory.__len__() >= self.batch_size:
+                    self.replay()
+                state = next_state
+                j += 1
                 # update second model
                 if j % self.c == 0:
-                    self.model2.coef_ = self.model.coef_
-                    self.model2.intercept_ = self.model.intercept_
+                    self.target.load_state_dict(self.model.state_dict())
+                    self.target.eval()
 
-            scores100.append(cum_reward)
             scores.append(cum_reward)
-            mean_score = np.mean(scores100)
+            mean_score = np.mean(scores)
+            mean_scores.append(mean_score)
             if e % 100 == 0 and self.debug:
-                print('[Episode {}] - Mean reward over last 100 episodes was {}.'.format(e, mean_score))
+                print('[Episode {}] - Mean reward {}.'.format(e, mean_score))
 
-        print('[Episode {}] - Mean Mean reward over last 100 episodes was {}.'.format(e, mean_score))
-        return scores
+        # noinspection PyUnboundLocalVariable
+        print('[Episode {}] - Mean reward {}.'.format(e, mean_score))
+        return scores, mean_scores
 
+    def save(self, path):
+        torch.save(self.model, path)
+
+    def load(self, path):
+        self.model = torch.load(path)
 
 if __name__ == '__main__':
-    agent = SGDPolyDualCartPoleSolver(gym.make('CartPole-v0'), debug=True)
+    agent = PolyDualCartPoleSolver(gym.make('CartPole-v0'), debug=True)
     agent.run()
